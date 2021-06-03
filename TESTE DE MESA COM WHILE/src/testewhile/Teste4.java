ecb7514ebda07d05d65b6097bca8164a97fbb349
luiz henrique <luizaaaljk2009@gmail.com>
2021-06-03 00:40:32 -0300

    TESTANDO O WHILE 4

---
+++ TESTE DE MESA COM WHILE/src/testewhile/Teste4.java
@@ -0,0 +1,24 @@
+package testewhile;
+
+import java.util.Scanner;
+
+public class Teste4 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		    int x = 0;
+		    int y;
+		    while(x < 5) {
+		    	y = x * 3;
+		    	System.out.println(y);
+		    	x = x + 2;
+		    }
+		    System.out.println("fim");
+		    
+
+	}
+
+}
